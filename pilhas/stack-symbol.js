c179c18b13397df0accba225e7c35aff376a9b9a
rafaelitajahy <rafaelitajahy@gmail.com>
2023-01-20 21:55:49 -0300

    feat: stack

---
+++ pilhas/stack-symbol.js
@@ -0,0 +1,53 @@
+const _items = Symbol('stackItems');
+
+class Stack {
+  constructor() {
+    this[_items] = [];
+  }
+
+  push(element) {
+    this[_items].push(element);
+  }
+
+  pop() {
+    return this[_items].pop();
+  }
+
+  peek() {
+    return this[_items][this[_items].length - 1];
+  }
+
+  isEmpty() {
+    return this[_items].length === 0;
+  }
+
+  size() {
+    return this[_items].length;
+  }
+
+  clear() {
+    this[_items] = [];
+  }
+
+  list() {
+    return this[_items];
+  }
+
+  print() {
+    console.log(this.toString());
+  }
+
+  toString() {
+    return this[_items].toString();
+  }
+}
+
+const stack = new Stack();
+stack.push(5);
+stack.push(8);
+let objectSymbols = Object.getOwnPropertySymbols(stack);
+console.log(objectSymbols.length);
+console.log(objectSymbols);
+console.log(objectSymbols[0]);
+stack[objectSymbols[0]].push(1);
+stack.print();
